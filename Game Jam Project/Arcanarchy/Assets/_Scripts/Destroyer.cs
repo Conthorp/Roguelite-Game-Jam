221c5e85afaf83681803b3919012e4a34a6cb255
Conthorp <dylanm.mcbrien@gmail.com>
2023-05-04 16:29:41 +0100

    Kieron Commit
    Some stuff

--- Game Jam Project/Arcanarchy/Assets/_Scripts/Destroyer.cs
+++ Game Jam Project/Arcanarchy/Assets/_Scripts/Destroyer.cs
@@ -6,6 +6,17 @@ public class Destroyer : MonoBehaviour
 {
     void OnTriggerEnter2D(Collider2D other)
     {
-        Destroy(other.gameObject);
+        switch (other.gameObject.tag)
+        {
+            case "Player":
+                break;
+            case "Enemy":
+                break;
+            case "Spell":
+                break;
+            default:
+                Destroy(other.gameObject);
+                break;
+        }
     }
 }
